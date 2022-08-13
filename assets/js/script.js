1c4d79c9bd29ac8e23169ac853b7e6c5cd13e63c
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-13 09:31:05 -0600

    Created function to handle background color, use Date() to compare time in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+////////////////    Variable declarations   //////////////////
 // Get Dom elements
 var rootEL = $("#root");
 var dateEl = $("#currentDay");
+var txtEl = $("textarea");
 
 
 
+////////////////    Functions   /////////////////////////
 // Function to display time
 function time() {
     // call Date() method use options to display time
@@ -14,6 +17,32 @@ function time() {
     rootEL.append(dateEl)
 }
 
+// Function to change color of background based on time
+function bgColor(){
+    // Get hour from Date()
+    var d = new Date();
+    // create array to compare values
+    var timeArray = [8,9,10,11,12,13,14,15,16,17]
+    
+    // Loop through array of times and compare to actual time
+    // getHour() returns int between 0-23
+    // Condition for past
+    for (var i =0; i<timeArray.length;i++){
+        if(timeArray[i] < d.getHours()){
+            txtEl.eq(i).addClass('past')
+        }
+        else if(timeArray[i] === d.getHours()){
+            txtEl.eq(i).addClass('present')
+        }
+        else if(timeArray[i] > d.getHours()){
+            txtEl.eq(i).addClass('future')
+        }
+    }
+}
 
-// Call setInterval method - call back function!!
+////////////////    Event Handlers      /////////////////////// 
+
+////////////////    Execute     ///////////////////////////// 
+// Call setInterval method to display the time- call back function!!
 setInterval(time,100)
+bgColor()
